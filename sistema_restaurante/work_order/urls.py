27774afee0473d24e84a83995dd14235582c0348
daceron96 <daceron96@gmail.com>
2021-12-15 14:23:52 -0500

    inicio pruebas peticion ajax

--- sistema_restaurante/work_order/urls.py
+++ sistema_restaurante/work_order/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import AddOrderView
+from .views import AddOrderView, prueba, peticion
 
 work_order_patterns = ([    
     #path product
     path('',AddOrderView.as_view(), name='order'),
+    path('prueba/',prueba.as_view(), name='prueba'),
+    path('peticion/',peticion.as_view(), name='peticion'),
 
 ],'order')
--- sistema_restaurante/work_order/views.py
+++ sistema_restaurante/work_order/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+import json
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
-
+from django.views.generic.list import ListView
 from .models import Order
-from product.models import Category
+from product.models import Category, Product
 
 class AddOrderView(TemplateView):
     template_name ='work_order/order.html'
@@ -11,3 +14,25 @@ class AddOrderView(TemplateView):
         context['categories'] = Category.objects.filter(status = True)
         return context
     
+class prueba(TemplateView):
+    template_name = 'work_order/prueba.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.filter(status = True)
+        return context
+
+
+
+class peticion(ListView):
+    model = Product
+    
+    def get_queryset(self):
+        return self.model.objects.filter(category__pk = self.request.GET['id_category']) 
+    
+    def get(self,request,*args,**kwargs):
+        if request.is_ajax():
+            return HttpResponse(serialize('json',self.get_queryset()), 'application/json')
+        else:
+            return redirect('order:prueba')
+        
+        #https://www.youtube.com/watch?v=nbSd0SuZrKs&ab_channel=Developer.pe
