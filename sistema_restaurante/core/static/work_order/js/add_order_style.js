2586649f6351f756f6d0e3a76e9da52f08e6c944
daceron96 <daceron96@gmail.com>
2021-12-16 15:30:10 -0500

    correccion de error modal ya lista y agregar items a la orden final falta editar y eliminar esos items

--- sistema_restaurante/core/static/work_order/js/add_order_style.js
+++ sistema_restaurante/core/static/work_order/js/add_order_style.js
@@ -1,5 +1,6 @@
-let query_data = []
 let detail_order = []
+let product = []
+let query_data = []
 /*Funcionalidad de disminuir la cantidad de productos a agregar en la lista final */
 function decrease_quantity(){
     let quantity = parseInt($('#quantity').val());
@@ -30,6 +31,8 @@ function increase_quantity(){
     if($('#btn_add_product').is(':disabled') && $('#quantity').val() == 1){
         $('#btn_add_product').prop('disabled',false);
     }
+    
+
 }
 
 /**funcion buscar lista de productos por categoria */
@@ -51,15 +54,21 @@ function list_product(id_category){
         success: function(data){
             if(data.length != 0){
                 for(let i=0; i < data.length; i++){
-                    let item ="<button type='button' class='list-group-item list-group-item-action' id='product_"+data[i]["fields"]["code"]+"' data-bs-toggle='modal' data-bs-target='#modal_add_product' onclick=complete_add_modal("+data[i]["fields"]["code"] +")><b>"+(i+1)+ ". </b>"+data[i]["fields"]["name"] +"</button>"
+                    let item ="<button " 
+                    +"class='list-group-item d-flex justify-content-between align-items-center' "
+                    +"id='product_"+data[i]["fields"]["code"]+"' "
+                    +"data-bs-toggle='modal' data-bs-target='#modal_add_product' "
+                    +"onclick=complete_add_modal("+data[i]["fields"]["code"] +")>"
+                    +data[i]["fields"]["name"]
+                    +"<span class='badge bg-primary rounded-pill px-2'>$ "+data[i]['fields']['sale_price']+"</span>"
+                    +"</button>"
                     $('#list_product').append(item)
                 }
             }else{
                 $('#list_product').append("<h2 class='text-center '><b>No existe ningun producto para esta categoria</b></h2>")
             }
             $('#btn_categories_list_'+id_category).addClass('active');
-            
-            query_data  = data;
+            query_data = data;
         }
     })
 }
@@ -68,20 +77,26 @@ function complete_add_modal(code_product){
     for(let i=0; i < query_data.length; i++){
         if(query_data[i]['fields']['code'] == code_product){
             $('#name_product').text(query_data[i]['fields']['name']);
-            $('#btn_add_product').click(add_order_detail(query_data[i]));
-            console.log(query_data[i])
+            product = query_data[i]['fields']
             break;
         }
     }
+    
 }
 
 /*Funcion que añade dinamicamente un producto a la lista final de pedido */
-function add_order_detail(product){
+function add_order_detail(){
     quantity = $('#quantity').val();
-    $("#product_list").append(
+    $("#detail_list").append(
         "<button class='list-group-item d-flex justify-content-between align-items-center' data-bs-toggle='modal'data-bs-target='#modal-item-add'>"
-        + product['fileds']['name']
+        + product['name']
         + "<span class='badge bg-primary rounded-pill px-4'>"+quantity+"</span>"
         + "</button>"
     );
+    detail_order.push(product)
+    product = []
+    $('#quantity').val('');
+    $('#btn_add_product').prop('disabled',true);
+    $('#btn_decrease_quantity').prop("disabled",true);
+
 }
